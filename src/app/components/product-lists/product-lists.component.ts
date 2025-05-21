1f3150f547fe166affebd4ca7a62e9776de6f9c8
debasis-nishank <devnsk.2024@gmail.com>
2025-05-21 11:01:47 +0530

    search functionality added

--- src/app/components/product-lists/product-lists.component.ts
+++ src/app/components/product-lists/product-lists.component.ts
@@ -11,11 +11,13 @@ import { PaginatedResponse } from '../../common/paginated-response';
 })
 export class ProductListsComponent {
   currentCatgoryId: number = 1;
+  currentCategoryName: string = "";
   products: Product[] = [];
   totalElements: number = 0;
-
+  searchMode: boolean = false;
   totalPages: number = 0;
   currentPage: number = 0;
+
 
   constructor(private productService: ProductServiceService,
 
@@ -31,6 +33,34 @@ export class ProductListsComponent {
   }
 
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    this.productService.searchProducts(theKeyword).subscribe(
+      {
+        next: (data: PaginatedResponse<Product>) => {
+          this.products = data.content;
+          this.totalElements = data.totalElements;
+          this.totalPages = data.totalPages;
+          this.currentPage = data.number;
+        },
+        error: (err) => {
+          console.error('Error fetching products:', err);
+          this.products = [];
+          this.totalElements = 0;
+          this.totalPages = 0;
+        }
+      }
+    )
+  }
+  //
+  handleListProducts() {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     //check if "id" param is available
 
@@ -38,20 +68,15 @@ export class ProductListsComponent {
     if (hasCategoryId) {
       //get the id and make into number
       this.currentCatgoryId = +this.route.snapshot.paramMap.get('id')!;
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     } else {
       // 
       this.currentCatgoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // now get the products by id
 
-    //   this.productService.getProductListByCategoryId(this.currentCatgoryId).subscribe(
-    //     data => {
-    //       console.log(data);
-    //       this.products = data;
-    //     }
-    //   )
-    // }
     this.productService.getProductListByCategoryId(this.currentCatgoryId).subscribe({
       next: (data: PaginatedResponse<Product>) => {
         this.products = data.content; // Extract the content array
