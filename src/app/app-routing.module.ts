1ee848fc0b1c7f4551b2b21463d08ce529b3b518
debasis-nishank <devnsk.2024@gmail.com>
2025-05-21 11:00:34 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListsComponent } from './components/product-lists/product-lists.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
   {
-    path: 'category/:id', component: ProductListsComponent
+    path: 'category/:id/:name', component: ProductListsComponent
   }, {
     path: 'category', component: ProductListsComponent
   },
   {
     path: 'products', component: ProductListsComponent
   },
+  {
+    path: 'search/:keyword', component: ProductListsComponent
+  }, {
+    path: 'category', component: ProductListsComponent
+  },
+  {
+    path: '', redirectTo: '/products', pathMatch: 'full'
+  }, {
+    path: '**', redirectTo: '/products', pathMatch: 'full'
+  },{
+    path:'products/:id',component:ProductDetailsComponent
+  }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListsComponent } from './components/product-lists/product-lists.component';
 import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListsComponent
+    ProductListsComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
