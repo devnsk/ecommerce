b9705365b97ae3936a76551457b561f55212187c
debasis-nishank <devnsk.2024@gmail.com>
2025-05-21 12:13:58 +0530

    id field added

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,5 +1,6 @@
 export class Product {
     constructor(
+       public id:number,
         public sku:string,
         public name:string,
         public description:string,
@@ -9,6 +10,7 @@ export class Product {
         public unitsInStock:number,
         public dateCreated:Date,
         public lastUpdated:Date
+        
     ){
         
     }
